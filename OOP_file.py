5582df269b89fceccac8a8c9ae05aa35f5920615
JOKERYGA <vlad.gluhovskiy_18376@mail.ru>
2024-02-13 12:30:50 +0700

    new decision for tast in stepik

--- OOP_file.py
+++ OOP_file.py
@@ -1,65 +1,36 @@
-from typing import Any
-
-
-class Course:
+class Museum:
     def __init__(self, name) -> None:
         self.name = name
-        self.modules = []
+        self.exhibits = []
+        
+    def add_exhibit(self, obj):
+        self.exhibits.append(obj)
 
-    def add_module(self, module):
-        self.modules.append(module)
+    def remove_exhibit(self, obj):
+        self.exhibits.remove(obj)
 
-    def remove_module(self, indx):
-        if 0 <= indx <= len(self.modules) - 1:
-            self.modules.pop(indx)
+    def get_info_exhibit(self, indx):
+        if 0 <= indx <= len(self.exhibits) - 1:
+            class_instance = self.exhibits[indx]
+            return f"Описание экспоната {class_instance.name}: {class_instance.descr}"
+                
 
-
-class Module:
-    def __init__(self, name) -> None:
+class Picture:
+    def __init__(self, name, author, descr):
         self.name = name
-        self.lessons = []
-
-    def add_lesson(self, lesson):
-        self.lessons.append(lesson)
-
-    def remove_lesson(self, indx):
-        if 0 <= indx <= len(self.lessons) - 1:
-            self.lessons.pop(indx)
+        self.author = author
+        self.descr = descr
 
 
-class LessonItem:
-    def __init__(self, title: str, practices: int, duration: int) -> None:
-        self.title = title
-        self.practices = practices
-        self.duration = duration
-
-    def __setattr__(self, name, value: Any) -> None:
-        if name == 'title' and not isinstance(value, str):
-            raise TypeError("Неверный тип присваиваемых данных для 'title'.")
-        elif name in ('practices', 'duration') and not isinstance(value, int):
-            raise TypeError(f"Неверный тип присваиваемых данных для '{name}'.")
-        elif name in ('practices', 'duration') and value < 0:
-            raise ValueError(f"Значение атрибута '{name}' не может быть отрицательным.")
-        else:
-            object.__setattr__(self, name, value)
-
-    def __getattr__(self, name) -> Any:
-        return False
-
-    def __delattr__(self, name: str) -> None:
-        if name in ('title', 'practices', 'duration'):
-            raise AttributeError("Запрещено удалять атрибуты title, practices и duration.")
-        object.__delattr__(self, name)
+class Mummies:
+    def __init__(self, name, location, descr):
+        self.name = name
+        self.location = location
+        self.descr = descr
 
 
-course = Course("Python ООП")
-module_1 = Module("Часть первая")
-module_1.add_lesson(LessonItem("Урок 1", 7, 1000))
-module_1.add_lesson(LessonItem("Урок 2", 10, 1200))
-module_1.add_lesson(LessonItem("Урок 3", 5, 800))
-course.add_module(module_1)
-module_2 = Module("Часть вторая")
-module_2.add_lesson(LessonItem("Урок 1", 7, 1000))
-module_2.add_lesson(LessonItem("Урок 2", 10, 1200))
-course.add_module(module_2)
-print(module_2.__dict__)
+class Papyri:
+    def __init__(self, name, date, descr):
+        self.name = name
+        self.date = date
+        self.descr = descr
