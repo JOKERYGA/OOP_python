96449e4ccfb37a247769938a70f5d24804a8976b
JOKERYGA <vlad.gluhovskiy_18376@mail.ru>
2024-02-14 16:21:16 +0700

    done task

--- OOP_file.py
+++ OOP_file.py
@@ -1,15 +1,48 @@
+from string import ascii_lowercase, digits
 from typing import Any
 
+class LoginForm:
+    def __init__(self, name, validators=None):
+        self.name = name
+        self.validators = validators
+        self.login = ""
+        self.password = ""
 
-class ImageFileAcceptor:
-    def __init__(self, extensions: tuple):
-        self.extensions = extensions
+    def post(self, request):
+        self.login = request.get('login', "")
+        self.password = request.get('password', "")
+
+    def is_validate(self):
+        if not self.validators:
+            return True
+
+        for v in self.validators:
+            if not v(self.login) or not v(self.password):
+                return False
+
+        return True
+
+
+# здесь прописывайте классы валидаторов: LengthValidator и CharsValidator
+class LengthValidator:
+    def __init__(self, min_length, max_length) -> None:
+        self.min_lenght = min_length
+        self.max_lenght = max_length
         
-    def __call__(self, filename) -> Any:
-        return any(filename.lower().endswith(ext) for ext in self.extensions)
+    def __call__(self, string) -> Any:
+        lenght = len(string)
+        return self.min_lenght <= lenght <= self.max_lenght
 
 
-filenames = ["boat.jpg", "web.png", "text.txt", "python.doc", "ava.jpg", "forest.jpeg", "eq_1.png", "eq_2.png"]
-acceptor = ImageFileAcceptor(('jpg', 'bmp', 'jpeg'))
-image_filenames = filter(acceptor, filenames)
-print(list(image_filenames))  # ["boat.jpg", "ava.jpg", "forest.jpeg"]
+class CharsValidator:
+    def __init__(self, chars) -> None:
+        self.chars = chars
+        
+    def __call__(self, string):
+        return all(char in self.chars for char in string)
+
+
+lg = LoginForm("Вход на сайт", validators=[LengthValidator(3, 50), CharsValidator(ascii_lowercase + digits)])
+lg.post({"login": "root", "password": "panda"})
+if lg.is_validate():
+    print("Дальнейшая обработка данных формы")
