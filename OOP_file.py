fcf894bd5486939a23921e2797fc41b8643f457b
JOKERYGA <vlad.gluhovskiy_18376@mail.ru>
2024-02-15 18:39:57 +0700

    new hard task

--- OOP_file.py
+++ OOP_file.py
@@ -1,16 +1,36 @@
-import sys
+import re
 
+class WordString:
+    def __init__(self, string="") -> None:
+        self.__string = string
 
-class Book:
-    def __init__(self, title: str, author: str, pages: int) -> None:
-        self.title = title
-        self.author = author
-        self.pages = pages
+    @property
+    def string(self):
+        return self.__string
+
+    @string.setter
+    def string(self, value):
+        # При установке значения свойства string
+        # производим преобразование строки в список слов
+        self.__string = value
 
     def __str__(self) -> str:
-        return f"Книга: {self.title}; {self.author}; {self.pages}"
+        return self.__string
 
+    def __len__(self):
+        # Преобразуем строку в список слов и возвращаем его длину
+        return len(re.findall(r'\b\w+\b', self.__string))
 
-lst_in = list(map(str.strip, sys.stdin.readlines()))
-book = Book(*lst_in)
-print(book)
+    def __call__(self, index):
+        words = re.findall(r'\b\w+\b', self.__string)
+        if 0 <= index < len(words):
+            return words[index]
+        else:
+            raise IndexError("Индекс выходит за пределы списка слов")
+
+words = WordString()
+words.string = "Курс по Python ООП"
+n = len(words)
+first = "" if n == 0 else words(0)
+print(words.string)
+print(f"Число слов: {n}; первое слово: {first}")
