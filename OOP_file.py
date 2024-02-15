38ea663315d012eb8cb85725bc967ceba05410bd
JOKERYGA <vlad.gluhovskiy_18376@mail.ru>
2024-02-15 21:55:04 +0700

    solved hard task to night

--- OOP_file.py
+++ OOP_file.py
@@ -1,49 +1,33 @@
-from math import sqrt
+import math
 
 
-class Complex:
-    def __init__(self, real, img) -> None:
-        self.real = real
-        self.img = img
-
-    @staticmethod
-    def correct_val(value):
-        if type(value) not in (int, float):
-            raise ValueError("Неверный тип данных.")
-
-    @property
-    def real(self):
-        return self._real
-
-    @real.setter
-    def real(self, value):
-        if isinstance(value, (int, float)):
-            self._real = value
+class RadiusVector:
+    def __init__(self, *args) -> None:
+        if len(args) == 1:
+            self.coordinates = [0] * args[0]
         else:
-            raise ValueError("Неверный тип данных для действительной части.")
+            self.coordinates = list(args)
 
-    @property
-    def img(self):
-        return self._img
-
-    @img.setter
-    def img(self, value):
-        if isinstance(value, (int, float)):
-            self._img = value
-        else:
-            raise ValueError("Неверный тип данных для мнимой части.")
-
+    def set_coords(self, *args):
+        for ind in range(len(args)):
+            if ind < len(self.coordinates):
+                self.coordinates[ind] = args[ind]
+    
+    def get_coords(self):
+        return tuple(self.coordinates)
+    
+    def __len__(self):
+        return len(self.coordinates)
+    
     def __abs__(self):
-        return sqrt(self.real**2 + self.img**2)
+        sum_of_squares = sum(coord ** 2 for coord in self.coordinates)
+        return math.sqrt(sum_of_squares)
 
 
-# Создание объекта класса Complex
-cmp = Complex(7, 8)
-
-# Установка новых значений для real и img через экземпляр класса
-cmp.real = 3
-cmp.img = 4
-
-# Вычисление модуля полученного комплексного числа
-c_abs = abs(cmp)
-print(c_abs)
+vector3D = RadiusVector(3)
+vector3D.set_coords(3, -5.6, 8)
+a, b, c = vector3D.get_coords()
+vector3D.set_coords(3, -5.6, 8, 10, 11) # ошибки быть не должно, последние две координаты игнорируются
+vector3D.set_coords(1, 2) # ошибки быть не должно, меняются только первые две координаты
+res_len = len(vector3D) # res_len = 3
+res_abs = abs(vector3D)
