c1950e5e7e3299dff2908b024abfa08bf2bad004
JOKERYGA <vlad.gluhovskiy_18376@mail.ru>
2024-02-15 21:07:48 +0700

    solved task

--- OOP_file.py
+++ OOP_file.py
@@ -1,4 +1,3 @@
-from typing import Any
 from math import sqrt
 
 
@@ -6,34 +5,34 @@ class Complex:
     def __init__(self, real, img) -> None:
         self.real = real
         self.img = img
-    
+
     @staticmethod
     def correct_val(value):
         if type(value) not in (int, float):
             raise ValueError("Неверный тип данных.")
-    
+
     @property
     def real(self):
         return self._real
-    
+
     @real.setter
     def real(self, value):
         if isinstance(value, (int, float)):
             self._real = value
         else:
             raise ValueError("Неверный тип данных для действительной части.")
-        
+
     @property
     def img(self):
         return self._img
-    
+
     @img.setter
     def img(self, value):
         if isinstance(value, (int, float)):
             self._img = value
         else:
             raise ValueError("Неверный тип данных для мнимой части.")
-    
+
     def __abs__(self):
         return sqrt(self.real**2 + self.img**2)
 
