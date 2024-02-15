5ee79d8ead829681e75b79b322304a832bec8a0a
JOKERYGA <vlad.gluhovskiy_18376@mail.ru>
2024-02-15 12:32:37 +0700

    edited document

--- OOP_file.py
+++ OOP_file.py
@@ -4,11 +4,11 @@ from typing import Any
 class RenderList:
     def __init__(self, type_list) -> None:
         self.type_list = type_list if type_list in {'ul', 'ol'} else "ul"
-        
+
     def __call__(self, lst) -> Any:
         if not lst:
             return ''
-        
+
         html = f"<{self.type_list}>\n"
         for item in lst:
             html += f"<li>{item}</li>\n"
