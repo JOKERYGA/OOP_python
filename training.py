cb177148bbb65d86c5525fcc4ab4b529602082e3
JOKERYGA <vlad.gluhovskiy_18376@mail.ru>
2024-02-18 16:22:43 +0700

    i misunderstand super() but i solved this task

---
+++ training.py
@@ -0,0 +1,17 @@
+class ListInteger(list):
+    def __init__(self, iterable=None):
+        if iterable is not None:
+            for item in iterable:
+                if not isinstance(item, int):
+                    raise TypeError("можно передавать только целочисленные значения")
+        super().__init__(iterable)
+
+    def __setitem__(self, index, value):
+        if not isinstance(value, int):
+            raise TypeError("можно передавать только целочисленные значения")
+        super().__setitem__(index, value)
+
+    def append(self, value):
+        if not isinstance(value, int):
+            raise TypeError("можно передавать только целочисленные значения")
+        return super().append(value)
